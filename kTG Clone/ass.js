81d2363fd140795d23924f3b4186cb57647a9d8a
Kode <108406480+KodeVoid@users.noreply.github.com>
2025-11-06 17:48:52 +0100

    Fix variable names and update element access methods

--- kTG Clone/ass.js
+++ kTG Clone/ass.js
@@ -1,20 +1,21 @@
 function showMessage() {
-    const firstName = document.getElementsByClassName(firstName).value;
-    const surName = document.getElementsByClassName(surName).value;
-    const age = document.getElementsByClassName(age).value;
-    const Gender = document.getElementsByClassName(Gender).value;    
+    const firstName = document.getElementsByClassName("firstname").value;
+    const surName = document.getElementsByClassName("surname").value;
+    const age = document.getElementsByClassName("age").value;
+    const gender = document.getElementsByClassName("gender").value;    
 
 
-const Message = 'HI, ${firstname} ${surName}, ${age} years old, ${gender}, welcome to our site!';
+const Message = 'HI, ${firstName} ${surName}, ${age} years old, ${gender}, welcome to our site!';
 
 document.getElementsByClassName("message").innerText = Message + "\nIf you want to continue click YES, if not click NO.";
 
-document.getElementsByClassName("YES").style.display = "inline-block";
-document.getElementsByClassName("NO").style.display = "inline-block";}
+document.getElementsById("YES").style.display = "inline-block";
+document.getElementsById("NO").style.display = "inline-block";}
 
 function continueSite() {
     alert("Great! Enjoy your stay.");}
 
 function exitSite(){
     alert("Goodbye! Enjoy Have a nice day.");}
+
 
